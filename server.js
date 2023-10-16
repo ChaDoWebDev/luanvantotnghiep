cd1e4e4ba743738f549665c11e41e7e22b72bc43
ChaDoWebDev <luongnguyen.011999@gmail.com>
2023-10-16 19:40:35 +0700

    server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require('http'); //nodejs 
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1'; //localhost
+const port = 8080;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.end('Hello World\n hoi dan it ');
 });
 
 server.listen(port, hostname, () => {
